6ae35d470f3c65b4eb681b5aba6b5967eac5a03a
achyutam26bce10159-droid <achyutam.26bce10159@vitbhopal.ac.in>
2026-09-25 06:34:20 +0530

    Update project4.py

--- project.py
+++ project.py
@@ -5,4 +5,10 @@ print()
 print("INVENTORY MANAGEMENT SYSTEM")
 print()
 print("OOOOOOOOOOOOOOOOOOOOOOOOOOOO")
-print("OOOOOOOOOOOOOOOOOOOOOOOOOOOO") 
+print("OOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+print()
+i=0
+E = {}
+print("1. Add new product") print("2. View all products") print("3. Search a product") print("4. Update product") print("5. Sell a product") print("6. Delete a product") print("7. Exit") print()
+X = int(input("enter your choice:"))
+print()
