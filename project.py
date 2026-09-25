92d6e18bc714588220c355814698e9a5a7cd3516
achyutam26bce10159-droid <achyutam.26bce10159@vitbhopal.ac.in>
2026-09-25 06:32:47 +0530

    Update project2.py

--- project.py
+++ project.py
@@ -1,1 +1,5 @@
- #Project-for-Python-essential-course #Inventory management system by achyutam sharma 26bce10159 
+ #Project-for-Python-essential-course #Inventory management system by achyutam sharma 26bce10159
+print("OOOOOOOOOOOOOOOOOOOOOOOOOOOO") print("OOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+print()
+print("INVENTORY MANAGEMENT SYSTEM")
+print() print("OOOOOOOOOOOOOOOOOOOOOOOOOOOO") print("OOOOOOOOOOOOOOOOOOOOOOOOOOOO") 
