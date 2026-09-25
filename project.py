3546c00458e281fd7d3a43e1d9e58b81122979fe
achyutam26bce10159-droid <achyutam.26bce10159@vitbhopal.ac.in>
2026-09-25 12:12:54 +0530

    Update project.py

--- project.py
+++ project.py
@@ -23,6 +23,47 @@ print("7. Exit")
 print()
 X = int(input("enter your choice:"))
 print()
+def viewing():
+  for j in range(0, len(E)):
+    print(E[j])
+    print()
+def searching():
+  N = int(input("enter index:"))
+  if N<len(E):
+    print(E[N])
+  else:
+    print("No product on this index")
+  print()
+def update():
+  a = int(input("enter index of product:"))
+  print("what do you want to update?")
+  print("1.Name, 2.Quantity, 3.price")
+  b = int(input("enter your choice"))
+  t = E[a]
+  if(b==1):
+    x = input("enter new name:")
+    t["name"] = x
+  if(b==2):
+    x = int(input("enter new quantity:"))
+    t["quantity"] = x
+  if(b==3):
+    x = input("enter new price:")
+    t["price"] = x
+  print("change is updated!")
+  print()
+def selling():
+  print("which product do you want to sell?")
+  a = int(input("enter index of product:"))
+  t = E[a]
+  t["quantity"] = t["quantity"] - 1
+  print("product is sold")
+  print()
+def delete():
+  print("which product do you want to delete?")
+  a = int(input("enter index of product:"))
+  E.pop(a)
+  print("product is deleted")
+  print()
 while(X<8):
   if(X==1):
     print("Adding new product")
